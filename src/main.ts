a9bc472eb9cb7e77161fc3c68face831bcd375d9
jonelantha <57597570+jonelantha@users.noreply.github.com>
2024-04-07 10:10:55 +0100

    Render one pixel

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 import "./style.css";
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+const width = 100;
+const height = 100;
+
+canvas.width = width;
+canvas.height = height;
+
+const imageData = new ImageData(width, height);
+const destBuffer = new Uint32Array(imageData.data.buffer);
+
+destBuffer[0] = 0xff00ff00;
+
+const context = canvas.getContext("2d")!;
+context.putImageData(imageData, 0, 0, 0, 0, width, height);
